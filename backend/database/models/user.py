b22b44c47ddb782e5804a7c9ba8afefa8a5b8c66
bernardoxdev <bernardocastro2006@gmail.com>
2026-06-08 16:55:38 -0300

    update: Atualizados modelos de dados para os mais atuais (#7)

--- backend/database/models/user.py
+++ backend/database/models/user.py
@@ -1,7 +1,13 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from backend.database.base import Base
+
+if TYPE_CHECKING:
+    from backend.database.models.estudante import Estudante
+    from backend.database.models.professor import Professor
 
 class User(Base):
     __tablename__ = "users"
@@ -11,8 +17,8 @@ class User(Base):
     nome: Mapped[str] = mapped_column(String, nullable=False)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
-    senha: Mapped[str] = mapped_column(String(255), nullable=True)
+    senha: Mapped[str] = mapped_column(String(255), nullable=False)
     role: Mapped[str] = mapped_column(String, nullable=False, default="user")
 
-    estudantes = relationship("Estudante", back_populates="user")
-    professores = relationship("Professor", back_populates="user")
+    estudantes: Mapped["Estudante"] = relationship("Estudante", back_populates="user")
+    professores: Mapped["Professor"] = relationship("Professor", back_populates="user")
